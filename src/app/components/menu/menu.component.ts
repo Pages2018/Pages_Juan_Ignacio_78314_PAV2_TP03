d5d008b3ed52a2e494b3a94de557b87fe46a5faa
Pages2018 <juanipages@gmail.com>
2022-06-12 18:51:22 -0300

    2 files modified

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  navbarOpen = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
 }
