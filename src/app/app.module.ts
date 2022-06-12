6e00d6ff0ec6888cdbee63f43321552a5fb2d502
Pages2018 <juanipages@gmail.com>
2022-06-12 18:46:02 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from '@angular/router';
 import {APP_BASE_HREF} from '@angular/common';  
-
+import {MenuComponent} from './components/menu/menu.component';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InicioComponent} from './components/inicio/inicio.component';
 import { ArticulosFamiliasComponent} from './components/articulos-familias/articulos-familias.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([{path:'menu', component: MenuComponent},
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: 'inicio', component: InicioComponent },
     { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
   ])
 ],
-  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent],
+  declarations: [MenuComponent, AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent ],
   bootstrap:    [ AppComponent ],
   providers: [{ provide: APP_BASE_HREF, useValue: "/" }]
 })
